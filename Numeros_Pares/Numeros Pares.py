6038efa3fbcc0ceb1cd7b2ccfb6b1997ade746b6
1chosen1 <1chosen1@hotmail.es>
2016-02-01 19:26:03 -0500

    Suma de numeros pares de 10 a 5000

---
+++ Numeros_Pares/Numeros Pares.py
@@ -0,0 +1,10 @@
+n = 10
+p = 0
+while n <= 5000:
+#print (n)
+    if n%2 == 0:
+        if n==100 or n==1000:
+            pass
+        p += n
+    n += 1
+print ("La suma de los numeros pares entre 10 y 5000 omitiendo los numeros 100 y 1000 es de: %i"% p)
